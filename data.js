0a5960b24761406851a2bfbd895027d57d75db51
Taihua Rubin <TaihuaRubin@gmail.com>
2020-07-25 12:01:38 -0400

    finished stack

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 Fill in your own code where you see "your code here".
@@ -11,19 +11,40 @@ will not need to edit the lines above and below them.
 
 class Stack {
   // Stack constructor function
-  constructor () {
-    // your code here
+  // stacks are LIFO -> last in, first out
+
+  constructor() {
+    this.stack = [];
+    this.stackTop = null; // start from null. lastest added item's index would be the new top
   }
 
   // Stack.prototype.add
-  add (item) {
-    // your code here
+  add(item) {
+    // check if the stack is empty
+    // if so, add the item to stack & make the stackTop 0
+    // if not, add item to stack & stackTop++
+    if (this.stack === []) {
+      this.stackTop = 0;
+      this.stack[this.stackTop] = item;
+      console.log("Im the first add", this.stack);
+    } else {
+      this.stackTop++;
+      this.stack[this.stackTop] = item;
+    }
+
     return this; // for chaining, do not edit
   }
 
   // Stack.prototype.remove
-  remove () {
-    // your code here
+  remove() {
+    // we want to take this.stack[this.stackTop out]
+    // make sthe stackTop -= 1 (if not 0, if 0 -> null)
+    // return the removed item
+    let removedItem = this.stack[this.stackTop];
+
+    this.stackTop === 0 ? (this.stackTop = null) : this.stackTop--;
+
+    return removedItem;
   }
 }
 
@@ -34,18 +55,18 @@ class Stack {
 
 class Queue {
   // Queue constructor function
-  constructor () {
+  constructor() {
     // your code here
   }
 
   // Queue.prototype.add
-  add (item) {
+  add(item) {
     // your code here
     return this; // for chaining, do not edit
   }
 
   // Queue.prototype.remove
-  remove () {
+  remove() {
     // your code here
   }
 }
@@ -57,30 +78,30 @@ class Queue {
 
 class LinkedList {
   // LinkedList constructor function
-  constructor () {
+  constructor() {
     this.head = this.tail = null;
   }
 
   // LinkedList.prototype.addToTail
-  addToTail (item) {
+  addToTail(item) {
     // your code here
     return this; // for chaining, do not edit
   }
 
   // LinkedList.prototype.removeFromTail
-  removeFromTail () {
+  removeFromTail() {
     // your code here
   }
 
   // LinkedList.prototype.forEach
-  forEach (callbackFunc) {
+  forEach(callbackFunc) {
     // your code here
   }
 }
 
 class ListNode {
   // ListNode constructor function
-  constructor (item, prev, next) {
+  constructor(item, prev, next) {
     this.item = item;
     this.next = next || null;
     this.prev = prev || null;
@@ -92,25 +113,25 @@ class ListNode {
 
 class Alist {
   // Alist constructor function
-  constructor () {
+  constructor() {
     // your code here
   }
 
   // Alist.prototype.set
-  set (key, value) {
+  set(key, value) {
     // your code here
     return this; // for chaining; do not edit
   }
 
   // Alist.prototype.get
-  get (key) {
+  get(key) {
     // your code here
   }
 }
 
 class AlistNode {
   // AlistNode constructor function
-  constructor (key, value, next) {
+  constructor(key, value, next) {
     this.key = key;
     this.value = value;
     this.next = next;
@@ -120,7 +141,7 @@ class AlistNode {
 //-----------------------------------------
 // Hash Tables
 
-function hash (key) {
+function hash(key) {
   let hashedKey = 0;
   for (let i = 0; i < key.length; i++) {
     hashedKey += key.charCodeAt(i);
@@ -130,19 +151,19 @@ function hash (key) {
 
 class HashTable {
   // HashTable constructor function
-  constructor () {
+  constructor() {
     this.buckets = Array(20);
     // your code here
   }
 
   // HashTable.prototype.set
-  set (key, value) {
+  set(key, value) {
     // your code here. DO NOT simply set a prop. on an obj., that's cheating!
     return this; // for chaining, do not edit
   }
 
   // HashTable.prototype.get
-  get (key) {
+  get(key) {
     // your code here. DO NOT simply get a prop. from an obj., that's cheating!
   }
 }
@@ -152,33 +173,33 @@ class HashTable {
 
 class BinarySearchTree {
   // BinarySearchTree constructor function
-  constructor (val) {
+  constructor(val) {
     // your code here
   }
 
   // BinarySearchTree.prototype.insert
-  insert (val) {
+  insert(val) {
     // your code here
     return this; // for chaining, do not edit
   }
 
   // BinarySearchTree.prototype.min
-  min () {
+  min() {
     // your code here
   }
 
   // BinarySearchTree.prototype.max
-  max () {
+  max() {
     // your code here
   }
 
   // BinarySearchTree.prototype.contains
-  contains (val) {
+  contains(val) {
     // your code here
   }
 
   // BinarySearchTree.prototype.traverse
-  traverse (callbackFunc) {
+  traverse(callbackFunc) {
     // your code here
   }
 }
